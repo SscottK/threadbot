be976cdd8298bad59145358e28ca5c6a73527fc0
SscottK <gr8whtninjassk@gmail.com>
2024-04-28 21:11:58 -0700

    made some changes

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import discord
 from discord import app_commands
 
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
@@ -26,6 +31,7 @@ dsc = "https://discord.com/channels/432275331794206720/1229111831159898284"
 vog = "https://discord.com/channels/432275331794206720/1229104380226179134"
 garden = "https://discord.com/channels/432275331794206720/1229100329539473519"
 
+channel_list = [432275331794206722, 685293052314517530]
 
 @tree.command(
     name="pve_damage",
@@ -33,7 +39,10 @@ garden = "https://discord.com/channels/432275331794206720/1229100329539473519"
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For more help with PvE Damage please see {pveDamage}")
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For more help with PvE Damage please see {pveDamage}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
 
 @tree.command(
     name="destiny-utilities",
@@ -41,7 +50,10 @@ async def pveDamage_command(interaction):
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"If you need help on your journey please see {destiny_utilities} for information ranging from Damage metas to encounter and activity guides.")
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"If you need help on your journey please see {destiny_utilities} for information ranging from Damage metas to encounter and activity guides.")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
 
 @tree.command(
     name="movement_tec",
@@ -49,7 +61,10 @@ async def pveDamage_command(interaction):
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For more information on the best Movement Tec please see {mvmnttech}")
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For more information on the best Movement Tec please see {mvmnttech}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
 
 @tree.command(
     name="pvp_utilities",
@@ -57,139 +72,192 @@ async def pveDamage_command(interaction):
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For the latest PvP metas check out {pvputilities}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For the latest PvP metas check out {pvputilities}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="warlards_ruin",
     description="A link to Warlords Ruin guides",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Warlords Ruin please see{warlordsruin}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Warlords Ruin please see{warlordsruin}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="ghosts_of_the_deep",
     description="A link to guides for Ghosts of the Deep dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Ghosts of the Deep dungeon please see{GotD}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Ghosts of the Deep dungeon please see{GotD}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="spire_of_the_watcher",
     description="A linkg to guides for Spire of the Watcher dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Spire of the Watcher please see{spire}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Spire of the Watcher please see{spire}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="root_of_nightmares",
     description="A link to guides for Root of Nightmares",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Root of Nightmares please see {RoN}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Root of Nightmares please see {RoN}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="grasp_of_avarice",
     description="A link to guides for Grasp of Avaraice dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Grasp of Avarice dungeon please see {grasp}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Grasp of Avarice dungeon please see {grasp}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="duality",
     description="A link to guides for Duality dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Duality dungeon please see {duality}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Duality dungeon please see {duality}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="prophecy",
     description="A link to guides for Prophecy dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Prophecy dungeon please see{prophecy}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Prophecy dungeon please see{prophecy}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="pit_of_heresy",
     description="A link to guides for Pit of Heresy dungeon",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Pit of Heresy dungeon please see{pit}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Pit of Heresy dungeon please see{pit}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="last_wish",
     description="A link to guides for Last Wish raid",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Last Wish raid please see {lastwish}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Last Wish raid please see {lastwish}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="shattered_throne",
     description="A link to guides for Shattered Throne dungeon ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Shattered throne please see {shatteredthrone}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Shattered throne please see {shatteredthrone}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="crotas_end",
     description="A link to guides for Crota's End raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Crota's End raid please see {crota}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Crota's End raid please see {crota}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="vow_of_the_disciple",
     description="A link to guides for Vow of the Disciple raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Vow of the Disciple raid please see {vow}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Vow of the Disciple raid please see {vow}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="kingsfall",
     description="A link to guides for Kingsfall raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Kingsfall raid please see {kingsfall}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Kingsfall raid please see {kingsfall}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="deep_stone_crypt",
     description="A link to guides for Deep Stone Crypt raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Deep Stone Crypt raid please see {dsc}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Deep Stone Crypt raid please see {dsc}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="vault_of_glass",
     description="A link to guides for Vault of Glass raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Vault of Glass raid please see {vog}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Vault of Glass raid please see {vog}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @tree.command(
     name="garden_of_salvation",
     description="A link to guides for Garden of Salvation raid ",
     guild=discord.Object(id=432275331794206720)
 )
 async def pveDamage_command(interaction):
-    await interaction.response.send_message(f"For help with Garden of Salvation raid please see {garden}")
-
+    if interaction.channel.id in channel_list:
+        await interaction.response.send_message(f"For help with Garden of Salvation raid please see {garden}")
+    else:
+        await interaction.response.send_message("You can't use this command in this channel", ephemeral=True)
+    
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=432275331794206720))
     print("Ready!")
-
-client.run("MTIzNDMwOTA1MDMyNjU4MTI4OQ.GyprOE.ZyHC5oBXijU07-rk0BHR5eGIDhjJsgDsHK-_pY")
+    
+    
+load_dotenv(find_dotenv())
+client.run(os.environ.get("TOKEN"))
